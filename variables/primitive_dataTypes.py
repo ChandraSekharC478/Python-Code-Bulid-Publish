cb6fac41812ea8aef7d2a045878d1cd4ff16f2d7
ChandraSekhar <Chandrasekharc784@gmail.com>
2025-03-19 22:10:29 +0530

    Primitive Data Types
    Primitive Data Types Learnt and explored

---
+++ variables/primitive_dataTypes.py
@@ -0,0 +1,21 @@
+#Primitive Data types 
+# 1. Number
+count = 5
+print (count);
+count=count+10;
+print(count);
+print ("***************")
+# 2. string
+first_name = 'chandhu'
+print (first_name)
+first_name="sekhar"+first_name
+print(first_name)
+print ("*******************")
+# float
+percentage = 1.5
+print(percentage)
+print(percentage+15)
+print ("********************")
+print (type(first_name))
+print(type(percentage))
+print(type(count))
